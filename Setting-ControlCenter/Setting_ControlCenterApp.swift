9bbd277991ffeefe9a3b9565090592ea9cbb60da
Minny27 <dltmdals0608@naver.com>
2023-03-06 09:35:47 +0900

    Initial Commit

---
+++ Setting-ControlCenter/Setting_ControlCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Setting_ControlCenterApp.swift
+//  Setting-ControlCenter
+//
+//  Created by SeungMin on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct Setting_ControlCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
